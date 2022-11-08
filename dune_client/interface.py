9f45bd959338649ec8a02b8e87674d92901eca00
Elisey Gusev <eliseygusev@gmail.com>
2022-11-08 15:03:52 +0100

    Abstract method to DuneInterface

--- dune_client/interface.py
+++ dune_client/interface.py
@@ -1,18 +1,19 @@
 """
 Abstract class for a basic Dune Interface with refresh method used by Query Runner.
 """
-from abc import ABC
+import abc
 
 from dune_client.models import ResultsResponse
 from dune_client.query import Query
 
 
 # pylint: disable=too-few-public-methods
-class DuneInterface(ABC):
+class DuneInterface(abc.ABC):
     """
     User Facing Methods for a Dune Client
     """
 
+    @abc.abstractmethod
     def refresh(self, query: Query) -> ResultsResponse:
         """
         Executes a Dune query, waits till query execution completes,
