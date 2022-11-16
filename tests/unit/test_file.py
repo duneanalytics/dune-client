85072dc791364014c166c1bca42a171e82fa9d15
Ben Smith <bh2smith@gmail.com>
2022-11-16 13:31:04 +0100

    generalize tests with detailed assertion error messaging

--- tests/unit/test_file.py
+++ tests/unit/test_file.py
@@ -33,47 +33,56 @@ class TestFileIO(unittest.TestCase):
             {"col1": "value01", "col2": "value02"},
             {"col1": "value11", "col2": "value12"},
         ]
+        self.file_manager = FileIO(TEST_PATH)
 
     def tearDown(self) -> None:
         cleanup()
 
-    def test_write_and_load(self):
-        file_manager = FileIO(TEST_PATH)
-        for file_type in FileType:
-            file_manager._write(self.dune_records, TEST_FILE, file_type)
-            loaded_records = file_manager._load(TEST_FILE, file_type)
-            self.assertEqual(self.dune_records, loaded_records)
+    def test_invertible_write_and_load(self):
+        for ftype in FileType:
+            self.file_manager._write(self.dune_records, TEST_FILE, ftype)
+            loaded_records = self.file_manager._load(TEST_FILE, ftype)
+            self.assertEqual(
+                self.dune_records,
+                loaded_records,
+                f"Assert invertible failed on {ftype}",
+            )
 
     def test_load_singleton(self):
-        file_manager = FileIO(TEST_PATH)
         for file_type in FileType:
-            file_manager._write(self.dune_records, TEST_FILE, file_type)
-            entry_0 = file_manager.load_singleton(TEST_FILE, file_type)
-            entry_1 = file_manager.load_singleton(TEST_FILE, file_type, 1)
-            self.assertEqual(self.dune_records[0], entry_0)
-            self.assertEqual(self.dune_records[1], entry_1)
+            self.file_manager._write(self.dune_records, TEST_FILE, file_type)
+            entry_0 = self.file_manager.load_singleton(TEST_FILE, file_type)
+            entry_1 = self.file_manager.load_singleton(TEST_FILE, file_type, 1)
+            self.assertEqual(
+                self.dune_records[0],
+                entry_0,
+                f"load_singletons failed on {file_type} at index 0",
+            )
+            self.assertEqual(
+                self.dune_records[1],
+                entry_1,
+                f"load_singletons failed on {file_type} at index 1",
+            )
 
     def test_skip_empty_write(self):
-        file_manager = FileIO(TEST_PATH)
         for file_type in FileType:
             with self.assertLogs():
-                file_manager._write([], TEST_FILE, file_type)
+                self.file_manager._write([], TEST_FILE, file_type)
             with self.assertRaises(FileNotFoundError):
-                file_manager._load(TEST_FILE, file_type)
+                self.file_manager._load(TEST_FILE, file_type)
 
     def test_idempotent_write(self):
-        file_manager = FileIO(TEST_PATH)
         for file_type in FileType:
-            file_manager._write(self.dune_records, TEST_FILE, file_type)
-            file_manager._write(self.dune_records, TEST_FILE, file_type)
+            self.file_manager._write(self.dune_records, TEST_FILE, file_type)
+            self.file_manager._write(self.dune_records, TEST_FILE, file_type)
             self.assertEqual(
-                self.dune_records, file_manager._load(TEST_FILE, file_type)
+                self.dune_records,
+                self.file_manager._load(TEST_FILE, file_type),
+                f"idempotent write failed on {file_type}",
             )
 
     def test_file_type(self):
-        for enum_instance in FileType:
-            self.assertEqual(enum_instance, FileType.from_str(str(enum_instance)))
-        # The above is equivalent to, but also covers new items when added.
-        # self.assertEqual(FileType.CSV, FileType.from_str("csv"))
-        # self.assertEqual(FileType.JSON, FileType.from_str("json"))
-        # self.assertEqual(FileType.NDJSON, FileType.from_str("ndjson"))
+        for file_type in FileType:
+            self.assertEqual(
+                file_type, FileType.from_str(str(file_type)), "failed on {file_type}"
+            )
