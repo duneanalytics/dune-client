b02b0e63fb12b45c9c5fc111d469b48cf78d73aa
Ben Smith <bh2smith@gmail.com>
2023-09-07 09:39:14 +0200

    fix bad rebase

--- dune_client/api/base.py
+++ dune_client/api/base.py
@@ -6,6 +6,7 @@ https://duneanalytics.notion.site/API-Documentation-1b93d16e0fa941398e15047f643e
 from __future__ import annotations
 
 import logging.config
+import os
 from json import JSONDecodeError
 from typing import Dict, Optional, Any
 
@@ -31,6 +32,15 @@ class BaseDuneClient:
         self.performance = performance
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
+
+    @classmethod
+    def from_env(cls) -> BaseDuneClient:
+        """
+        Constructor allowing user to instantiate a client from environment variable
+        without having to import dotenv or os manually
+        We use `DUNE_API_KEY` as the environment variable that holds the API key.
+        """
+        return cls(os.environ["DUNE_API_KEY"])
 
     @property
     def api_version(self) -> str:
