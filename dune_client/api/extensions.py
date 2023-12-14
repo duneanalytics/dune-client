aa3aebfd92aa1672a53e12cb94b0677159e6d298
Antonio Mendes <soeiromendes.antonio@gmail.com>
2023-12-14 10:41:18 +0000

    linting

--- dune_client/api/extensions.py
+++ dune_client/api/extensions.py
@@ -206,7 +206,11 @@ class ExtendedAPI(ExecutionAPI, QueryAPI):
         Requires premium subscription!
         """
         query = self.create_query(name, query_sql, params, is_private)
-        results = self.run_query(query=query.base, performance=performance, ping_frequency=ping_frequency)
+        results = self.run_query(
+            query=query.base,
+            performance=performance,
+            ping_frequency=ping_frequency
+            )
         if archive_after:
             self.archive_query(query.base.query_id)
         return results
