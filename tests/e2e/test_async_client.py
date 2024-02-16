903e0bf3bb5c41b0887f0f08b77577b2ad9a139a
Richard Keo <richard@dune.com>
2024-02-16 17:28:44 +0000

    Add tests

--- tests/e2e/test_async_client.py
+++ tests/e2e/test_async_client.py
@@ -3,6 +3,7 @@ import unittest
 
 import aiounittest
 import dotenv
+import pandas
 
 from dune_client.client_async import AsyncDuneClient
 from dune_client.query import QueryBase
@@ -11,6 +12,10 @@ from dune_client.query import QueryBase
 class TestDuneClient(aiounittest.AsyncTestCase):
     def setUp(self) -> None:
         self.query = QueryBase(name="Sample Query", query_id=1215383)
+        self.multi_rows_query = QueryBase(
+            name="Query that returns multiple rows",
+            query_id=3435763,
+        )
         dotenv.load_dotenv()
         self.valid_api_key = os.environ["DUNE_API_KEY"]
 
@@ -32,6 +37,44 @@ class TestDuneClient(aiounittest.AsyncTestCase):
         async with AsyncDuneClient(self.valid_api_key) as cl:
             results = (await cl.refresh(self.query)).get_rows()
         self.assertGreater(len(results), 0)
+
+    async def test_refresh_with_pagination(self):
+        # Arrange
+        async with AsyncDuneClient(self.valid_api_key) as cl:
+
+            # Act
+            results = (await cl.refresh(self.multi_rows_query, batch_size=1)).get_rows()
+
+        # Assert
+        self.assertEqual(
+            results,
+            [
+                {"number": 1},
+                {"number": 2},
+                {"number": 3},
+                {"number": 4},
+                {"number": 5},
+            ],
+        )
+
+    async def test_refresh_csv_with_pagination(self):
+        # Arrange
+        async with AsyncDuneClient(self.valid_api_key) as cl:
+
+            # Act
+            result_csv = await cl.refresh_csv(self.multi_rows_query, batch_size=1)
+
+        # Assert
+        self.assertEqual(
+            pandas.read_csv(result_csv.data).to_dict(orient="records"),
+            [
+                {"number": 1},
+                {"number": 2},
+                {"number": 3},
+                {"number": 4},
+                {"number": 5},
+            ],
+        )
 
     @unittest.skip("Large performance tier doesn't currently work.")
     async def test_refresh_context_manager_performance_large(self):
