92254e1759894cc2008f77244607d0f95051d96b
Philipp Wassibauer <philipp.wassibauer@gmail.com>
2024-06-19 14:36:17 +0100

    add basic integration testing for custom endpoints

---
+++ tests/e2e/test_custom_endpoints.py
@@ -0,0 +1,25 @@
+import copy
+import os
+import time
+import unittest
+
+import dotenv
+
+from dune_client.client import DuneClient
+
+dotenv.load_dotenv()
+
+
+class TestCustomEndpoints(unittest.TestCase):
+    def setUp(self) -> None:
+        self.valid_api_key = os.environ["DUNE_API_KEY"]
+
+    def test_get_execution_status(self):
+        dune = DuneClient(self.valid_api_key)
+        results = dune.get_custom_endpoint_result("dune", "new-test")
+        self.assertEqual(len(results.get_rows()), 10)
+        self.assertEqual(len(results.get_columns()), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
